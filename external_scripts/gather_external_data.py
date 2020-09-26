7ddea4eb95d596028e2da4cb545db8ec5fa1dc5a
jbar-ry <james.barry26@mail.dcu.ie>
2020-09-26 16:34:10 +0100

    update paths to initial files

--- external_scripts/gather_external_data.py
+++ external_scripts/gather_external_data.py
@@ -13,19 +13,22 @@ if not os.path.exists(target_data_path):
     print(f"Creating directory at: {target_data_path}")
     os.makedirs(target_data_path)
 
+
+found_files = 0
+copied_files = 0
+
 if os.path.exists(ga_data_dir):
     print(f"Found directory containing Irish files at {ga_data_dir}")
     for corpus in os.listdir(ga_data_dir):
-        print(f"Found {corpus}")
         if corpus in external_corpora:
+            print(f"Found {corpus}")
+            file_path = corpus + "/processed"
 
-            if corpus == "gdrive":
-                corpus += "/gathered"
-
-            data_path = os.path.join(ga_data_dir, corpus)
+            data_path = os.path.join(ga_data_dir, file_path)
             print(f"Copying Irish data from: {data_path}")
             
             for f in os.listdir(data_path):
+                found_files += 1
                 # if the file is a compressed file which has already been downloaded
                 # copy it to the target directory
                 if f.endswith(".bz2") or f.endswith(".gz"):
@@ -33,6 +36,9 @@ if os.path.exists(ga_data_dir):
                     original_file = os.path.join(data_path, f)
                     target_file = os.path.join(target_data_path, f)
                     copyfile(original_file, target_file)
+                    copied_files += 1
+
+        print(f"copied {(copied_files / found_files) * 100}% of files")
 else:
     print(f"Could not find Irish data directory, tried: {ga_data_dir}")
 
