251a4ebeb3768ea99e053b48c4e0e56d0ffdbfeb
jbar-ry <james.barry26@mail.dcu.ie>
2020-11-23 18:41:51 +0000

    tidy up print statements

--- external_scripts/gather_external_data.py
+++ external_scripts/gather_external_data.py
@@ -3,7 +3,6 @@ import sys
 from shutil import copyfile
 import subprocess
 from argparse import ArgumentParser
-
 import fileinput
 
 
@@ -14,7 +13,7 @@ def argparser():
     parser.add_argument(
         '--datasets',
         type=str,
-        help='Specify the datasets to download.',
+        help='Specify the datasets to copy.',
         choices={
             'conll17',
             'gdrive',
@@ -24,11 +23,12 @@ def argparser():
         },
         nargs='+',
     )
-    parser.add_argument('--filter-threshold', type=str,
+    parser.add_argument('--filter-type', type=str,
         choices={
-            '0',
-            '05',
-            '08',
+            'none',
+            'basic',
+            'basic_0.5',
+            'basic_0.8',
             })
     parser.add_argument('--input-type', type=str,
         choices={
@@ -42,32 +42,45 @@ def argparser():
 def main(argv):
     args = argparser().parse_args(argv[1:])
     corpora_string = "_".join(args.datasets)
+    run_string = f'{corpora_string}_filtering_{args.filter_type}'
+    print(f"copying data for run: {run_string}")
 
     ga_data_dir = os.path.join('..', 'Irish-BERT/data/ga')
 
-    target_data_path = os.path.join('data', corpora_string, 'ga', 'external-texts')
+    target_data_path =  os.path.join('data', run_string, 'ga', 'external-texts')
     if not os.path.exists(target_data_path):
         print(f"Creating directory at: {target_data_path}")
         os.makedirs(target_data_path)
 
-    data_path = f'DATA_DIR=$(pwd_relative_path "$BASE_DIR/data/{corpora_string}")'
+    
+    data_path = f'DATA_DIR=$(pwd_relative_path "$BASE_DIR/data/{corpora_string}_filtering_{args.filter_type}")'
     external_path = f'EXTERNAL_CORPORA_DIR="$DATA_DIR/ga/external-texts"'
 
-    for line in fileinput.input("pipeline/common.sh", inplace=1):
+    # take the default config file and alter it based on the specific data/filtering type.
+    # this is necessary because it means environment paths won't change if the pipeline is being run for multiple configurations simultaneously.
+    DEFAULT_CONFIG="pipeline/default.sh"
+    SPECIFIC_CONFIG=f"pipeline/common_{run_string}.sh"
+    print(f"creating config: {SPECIFIC_CONFIG}")
+    copyfile(DEFAULT_CONFIG, SPECIFIC_CONFIG)
+
+    for line in fileinput.input(SPECIFIC_CONFIG, inplace=1):
         new_line_symbol = line.rfind('\n')
         line = line[:new_line_symbol]
+        # replace placeholder data dir with specific data dir
         if line == 'DATA_DIR=$(pwd_relative_path "$BASE_DIR/data")':
             line = line.replace(line, data_path)
             print(line)
+        # set external corpora dir based on new data dir
         elif line == 'EXTERNAL_CORPORA_DIR=""':
             line = line.replace(line, external_path)
             print(line)
+        # keep line unchanged
         else:
             print(line)
 
-
     found_files = 0
     copied_files = 0
+
     if os.path.exists(ga_data_dir):
         print(f"Found directory containing Irish files at {ga_data_dir}")
         for corpus in os.listdir(ga_data_dir):
@@ -87,11 +100,9 @@ def main(argv):
                         copyfile(original_file, target_file)
                         copied_files += 1
 
-                print(f"copied {(copied_files / found_files) * 100}% of files")
+                print(f"copied {(copied_files / found_files) * 100}% of files for {corpus}")
     else:
         print(f"Could not find Irish data directory, tried: {ga_data_dir}")
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
-
-
